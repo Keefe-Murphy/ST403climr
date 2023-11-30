6dcf09f532f91ee1712f9f575170a3492bcd96d6
Keefe-Murphy <keefe.murphy@mu.ie>
2023-11-30 13:26:56 +0000

    Added Roxygen documentation to three main functions & created NAMESPACE. N.B.: intentionally added some silly mistakes so that I can better demonstrate devtools::check() in the next class.

--- R/plot_climr.R
+++ R/plot_climr.R
@@ -1,3 +1,24 @@
+#' Plot climr output
+#'
+#' Plots different types of fitted models for global or hemispheric data from NASA at different intervals.
+#'
+#' @param x An object of class \code{"climr_fit"} outputted from the \code{link{fit}} function.
+#' @param time_grid An optional time grid over which to produce fitted values of the model. Defaults to an interval of length \code{100}.
+#' @param ... Catches unused arguments to \code{plot} (not currently implemented).
+#'
+#' @return Nothing, just a nice ggplot.
+#'
+#' @export
+#' @author Keefe Murphy - <\email{keefe.murphy@@mu.ie}>
+#' @seealso \code{\link{fit}}, \code{\link{load_climr}}
+#' @examples
+#' dat <- load_climr(type = "SH")
+#' mod1 <- fit(dat)
+#' mod2 <- fit(dat, data_type="monthly", fit_type="smooth.spline")
+#' mod3 <- fit(dat, data_type="quarterly", fit_type="loess")
+#' plot(mod1)
+#' plot(mod2)
+#' plot(mod3)
 plot.climr_fit <- function(x, time_grid = pretty(x$data$x, n=100), ...) {
 
   ## Create a nice plot from the output of fit.climr()
@@ -8,12 +29,12 @@ plot.climr_fit <- function(x, time_grid = pretty(x$data$x, n=100), ...) {
   ## Get some predicted values based on the time grid and fit_type
   fits <- switch(x$fit_type,
                  lm = {
-                   tibble(time_grid, pred=predict(x$model,
-                                                  newdata=tibble(x=time_grid)))
+                   tibble(time_grid, pred=stats::predict(x$model,
+                                                         newdata=tibble(x=time_grid)))
                  },
                  loess = {
-                   tibble(time_grid, pred=predict(x$model,
-                                                  newdata=tibble(x=time_grid))) |> stats::na.omit()
+                   tibble(time_grid, pred=stats::predict(x$model,
+                                                         newdata=tibble(x=time_grid))) |> stats::na.omit()
                  },
                  smooth.spline = {
                    tibble(time_grid, pred=predict(x$model, tibble(time_grid))$y[,1])
